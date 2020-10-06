e49a0bafafcbf120e32af6e5a92bd2fa791ee1e1
Khasbiev <Khasbiich@yandex.ru>
2020-10-06 16:10:58 +0300

    init: start task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    if(n < 0){
+        return String(n*-1).split('').reverse().join('')/1;
+     } else {
+        return String(n).split('').reverse().join('')/1;
+     }
 }
